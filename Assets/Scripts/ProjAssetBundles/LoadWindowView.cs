20088f071e4148d360ed47654515fbb7fb8bdad5
Maleriya <50017431+Maleriya@users.noreply.github.com>
2022-03-13 17:03:59 +0300

    Проект с Asset Bundle и Adressables

---
+++ Assets/Scripts/ProjAssetBundles/LoadWindowView.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
+
+public class LoadWindowView : AssetBundleViewBase
+{
+    [SerializeField]
+    private Button _loadAsssetsButton;
+    [SerializeField]
+    private Button _loadAddressableButton;
+
+    [SerializeField]
+    private AssetReference _loadPrefab;
+
+    [SerializeField]
+    private RectTransform _rectTransform;
+
+    private List<AsyncOperationHandle<GameObject>> addressablePrefabs = new List<AsyncOperationHandle<GameObject>>();
+
+    private void Start()
+    {
+        _loadAsssetsButton.onClick.AddListener(LoadAsset);
+        _loadAddressableButton.onClick.AddListener(CreateAddressableBrefab);
+    }
+
+    private void CreateAddressableBrefab()
+    {
+        var addressablePrefab = Addressables.InstantiateAsync(_loadPrefab, _rectTransform);
+        addressablePrefabs.Add(addressablePrefab);
+    }
+
+    private void OnDestroy()
+    {
+        _loadAsssetsButton.onClick.RemoveAllListeners();
+
+        foreach (var addressablePrefab in addressablePrefabs)
+        {
+            Addressables.ReleaseInstance(addressablePrefab);
+        }
+
+        addressablePrefabs.Clear();
+    }
+
+    private void LoadAsset()
+    {
+        _loadAsssetsButton.interactable = false;
+
+        StartCoroutine(DownloadAndSetAssetBundle());
+    }
+
+}
